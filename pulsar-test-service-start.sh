60845d056d27f9183a01083f01f3e753bd42580e
Yunze Xu <xyzinfernity@163.com>
2022-12-22 17:14:50 +0800

    [TEST] Verify Pulsar 2.11.0 rc3

--- pulsar-test-service-start.sh
+++ pulsar-test-service-start.sh
@@ -25,7 +25,7 @@ cd $SRC_DIR
 
 ./pulsar-test-service-stop.sh
 
-CONTAINER_ID=$(docker run -i -p 8080:8080 -p 6650:6650 -p 8443:8443 -p 6651:6651 --rm --detach apachepulsar/pulsar:latest sleep 3600)
+CONTAINER_ID=$(docker run -i -p 8080:8080 -p 6650:6650 -p 8443:8443 -p 6651:6651 --rm --detach mattison/pulsar:2.11.0-rc3 sleep 3600)
 echo $CONTAINER_ID > .tests-container-id.txt
 
 docker cp test-conf $CONTAINER_ID:/pulsar/test-conf
